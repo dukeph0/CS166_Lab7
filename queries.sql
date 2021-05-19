39b438f8fad4dd2ee5444f9ecb176e4deb5002dd
dukeph0 <dpham5161@outlook.com>
2021-05-19 09:33:12 -0700

    Update queries.sql

---
+++ queries.sql
@@ -0,0 +1,16 @@
+--Count how many parts in NYC have more than 70 parts on hand
+
+
+--Count how many total parts on hand, in both NYC AND SFO, are Red
+
+
+--List all the suppliers that have more total on_hand parts in NYC than they do in SFO.
+
+
+--List all the suppliers that supply parts in NYC that aren't supplied by anyone in SFO.
+
+
+--Update all of the NYC on_hand values to on_hand - 10.
+
+
+--Delete all parts from NYC which have less than 30 parts on_hand.
