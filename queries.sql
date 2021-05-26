d19d0f2618237dbec19d8df65949a992a019ee0a
dukeph0 <dpham5161@outlook.com>
2021-05-26 00:24:10 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -18,12 +18,24 @@ WHERE PN.color = 'Red';
 SELECT COUNT(PS.on_hand) AS ps_hand
 FROM part_sfo PS
 WHERE PS.color = 'Red';
-SELECT pn_hand + ps_hand; --without index
+SELECT (pn_hand + ps_hand); --without index
 
-
+SELECT COUNT(*) AS pn_hand2
+FROM part_nyc
+WHERE color = 'Red';
+SELECT COUNT(*) AS ps_hand2
+FROM part_sfo
+WHERE color = 'Red';
+SELECT (pn_hand2 + ps_hand2); --with index
 
 --List all the suppliers that have more total on_hand parts in NYC than they do in SFO.
+SELECT S.supplier_name 
+FROM supplier S, part_nyc PN, part_sfo PS
+WHERE COUNT(PN.on_hand) > COUNT(PS.on_hand); --without index
 
+SELECT S.supplier_name
+FROM supplier S, part_nyc PN, part_sfo PS
+WHERE COUNT(PN.*) > COUNT(PS.*); --with index
 
 --List all the suppliers that supply parts in NYC that aren't supplied by anyone in SFO.
 
