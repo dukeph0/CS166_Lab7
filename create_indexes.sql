daa05d392ab2c77598bdae05a63bcaa64be8d138
dukeph0 <dpham5161@outlook.com>
2021-05-26 01:50:35 -0700

    Update create_indexes.sql

---
+++ create_indexes.sql
@@ -0,0 +1,23 @@
+CREATE INDEX part1_id
+ON lab7
+(result);
+
+CREATE INDEX part2_id 
+ON lab7
+(result);
+
+CREATE INDEX part3_id
+ON lab7
+(result);
+
+CREATE INDEX part4_id
+ON lab7
+(result);
+
+CREATE INDEX part5_id
+ON lab7
+(result);
+
+CREATE INDEX part6_id
+ON lab7
+(result);
